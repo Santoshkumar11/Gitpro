c40d2f4f68bae0063de3a92d4e20f2ee185394ea
Santoshkumar11 <31270861+Santoshkumar11@users.noreply.github.com>
2018-01-09 14:40:43 +0530

    Add files via upload

---
+++ AddNumbers.java
@@ -0,0 +1,12 @@
+class AddNumbers
+{
+   public static void main(String args[])
+   {
+      int x, y, z;
+      System.out.println("Enter two integers to calculate their sum ");
+      x = 7;
+      y = 6;
+      z = x + y;
+      System.out.println("Sum of entered integers = "+z);
+   }
+}
